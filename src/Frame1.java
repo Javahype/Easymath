5d6a2179c9edff9027f464a730ad842d68d1db50
smets95 <sandermets95@hotmail.com>
2015-12-13 18:40:23 +0200

    updated comments

--- src/Frame1.java
+++ src/Frame1.java
@@ -11,18 +11,17 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+
+
 
 public class Frame1 {
 
 	private JFrame frame;
 
-	/**
-	 * Launch the application.
-	 */
+	// the launch of the application
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -36,18 +35,16 @@ public class Frame1 {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
+	// creating the application
+	
 	public Frame1() {
 		initialize();}
 		
 	
 
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
+	//frame contents 
+	
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.DARK_GRAY);
